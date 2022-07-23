06ca2c36babf169fb536675e18f4f6b8253da65d
Raphael Gomes <raphaelfsg@gmail.com>
2022-07-23 12:14:02 -0300

    teste

---
+++ streamlit_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+
+#Título do aplicativo
+st.title('Uber pickups in NYC')
+
+# Carregando o onjunto de dados de embarques e desembarques do Uber em Nova York
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+
+data = load_data(10000)
+
+data_load_state.text("Done! (using st.cache)")
